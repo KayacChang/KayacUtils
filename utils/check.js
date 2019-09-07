f0c71d1cb26fc96347c7d2bb396e68d07420ab01
KayacChang <egg734631@gmail.com>
2019-09-07 14:18:10 +0800

    Add lodash type check

---
+++ utils/check.js
@@ -0,0 +1,10 @@
+export isArray from 'lodash/isArray';
+export isNumber from 'lodash/isNumber';
+export isFunction from 'lodash/isFunction';
+export isString from 'lodash/isString';
+export isSymbol from 'lodash/isSymbol';
+
+
+export isError from 'lodash/isError';
+export isEqual from 'lodash/isEqual';
+
