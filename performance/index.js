d71d18170c9450be8fcc0576c6c3de3706239571
KayacChang <egg734631@gmail.com>
2019-09-07 15:37:27 +0800

    Add throttle by

--- performance/index.js
+++ performance/index.js
@@ -4,3 +4,19 @@ import debounce from 'lodash/debounce';
 
 export * from './pageLoad';
 export {throttle, debounce};
+
+export function throttleBy(func) {
+    let skip = false;
+
+    return async function call(...args) {
+        if (skip) return;
+
+        skip = true;
+
+        const result = await func(...args);
+
+        skip = false;
+
+        return result;
+    };
+}
