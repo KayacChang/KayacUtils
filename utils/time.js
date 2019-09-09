2f6add55a6b660eefa179f7f00899ecffbbe0803
KayacChang <egg734631@gmail.com>
2019-09-09 11:38:37 +0800

    Add

--- utils/time.js
+++ utils/time.js
@@ -15,4 +15,14 @@ export function nextFrame() {
     return new Promise((r) => requestAnimationFrame(r));
 }
 
+/**
+ * Return a function which represents the time elapsed since Timer get call.
+ * @return {function(): number} milliseconds.
+ */
+export function timer() {
+    const start = performance.now();
 
+    return function get() {
+        return performance.now() - start;
+    };
+}
